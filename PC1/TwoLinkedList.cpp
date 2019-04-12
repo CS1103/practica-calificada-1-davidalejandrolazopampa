dd07df5e6378bfaebaa7b82f1a17af5811a911bf
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-04-12 13:36:40 -0500

    pOST

--- PC1/TwoLinkedList.cpp
+++ PC1/TwoLinkedList.cpp
@@ -2,4 +2,97 @@
 // Created by ruben on 4/12/19.
 //
 
+#include <string>
+#include <iostream>
+#include <fstream>
 #include "TwoLinkedList.h"
+#include <string>
+using namespace std;
+
+void UTEC::TwoLinkedList::push_back1(int value) {
+    Node* temp=new Node(value);
+    if(empty()){
+    tail=temp;
+    head1=temp;
+    } else{
+        if (head1 == nullptr && tail1 == nullptr) {
+            tail1 = temp;
+            head1 = temp;
+        } else {
+            tail1->next = temp;
+            tail1 = temp;
+        }
+    }
+}
+void UTEC::TwoLinkedList::push_back2(int value) {
+    Node* temp=new Node(value);
+    if(empty()){
+        tail=temp;
+        head2=temp;
+    } else {
+        if (head2 == nullptr && tail2 == nullptr) {
+            tail2 = temp;
+            head2 = temp;
+        } else {
+            tail2->next = temp;
+            tail2 = temp;
+
+        }
+}
+void UTEC::TwoLinkedList::print1(){
+    if ( head1 == tail1){
+        std::cout << head1 << "  " << tail1 << std::endl;
+    }
+    else{
+
+        node* temp = head1;
+        while(temp->next != nullptr){
+            std::cout << temp->value << std::endl;
+            temp = temp->next;}
+        std::cout << temp->value << std::endl;
+    }
+    if ( head2 == tail2){
+        std::cout << head2 << "  " << tail2 << std::endl;
+    }
+    else{
+
+        node* temp = head2;
+        while(temp->next != nullptr){
+            std::cout << temp->value << std::endl;
+            temp = temp->next;}
+        std::cout << temp->value << std::endl;
+    }
+}
+
+
+void UTEC::TwoLinkedList::getlist(int value){
+string merge="El numero es: "+to_string(value);
+}
+void UTEC::TwoLinkedList::search(int value){
+
+}
+void UTEC::TwoLinkedList::save(string contenido2){
+    ofstream fs(contenido2.c_str());
+    fs << head1 <<", "<< endl;
+    fs.close();
+    cout << "El archivo ha sido creado correctamente" << endl;
+    system("pause");
+}
+void UTEC::TwoLinkedList::load(string contenido2){
+    ifstream fs(contenido2.c_str(), ios:: in );
+    char linea[128];
+    long contador = 0L;
+    if (fs.fail())
+        cerr << "El fichero no existe" << endl;
+    else
+        while (!fs.eof()) {
+            fs.getline(linea, sizeof(linea));
+            cout << linea << endl;
+            if ((++contador % 24) == 0) {
+                cout << "continuar...";
+                cin.get();
+            }
+        }
+    fs.close();
+    system("pause");
+}
--- PC1/TwoLinkedList.h
+++ PC1/TwoLinkedList.h
@@ -5,10 +5,37 @@
 #ifndef PC1_TWOLINKEDLIST_H
 #define PC1_TWOLINKEDLIST_H
 
+using namespace std;
+namespace UTEC{
 
-class TwoLinkedList {
-
-};
+    struct Node{
+        int value;
+        Node* next;
+        Node(int value):value{value},next{ nullptr}{}
+    };
+    class TwoLinkedList {
+        Node* head1;
+        Node* head2;
+        Node* tail;
+        Node* tail1;
+        Node* tail2;
+    public:
+        TwoLinkedList(): head1(nullptr), head2(nullptr),tail(nullptr),tail1(nullptr),tail2(nullptr){}
+        void push_back1(int value);
+        void push_back2(int value);
+        bool empty();
+        string merge(int value);
+        bool is_merge();
+        void getlist(int value);
+        int size();
+        void print1();
+        void print2();
+        void search(int value);
+        void save(string contenido2);
+        void load(string contenido2);
+        ~TwoLinkedList();
+    };
+}
 
 
 #endif //PC1_TWOLINKEDLIST_H
